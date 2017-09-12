e57c13da73b162d92261125fad875884a0ac50f7
shahronak1996@gmail.com <Ronak@MacBook-Air-2.attlocal.net>
2017-09-12 01:10:44 -0500

    Added nav arrows

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import '../layouts/index.scss';
+import { Icon } from 'semantic-ui-react'
 
 const IndexPage = () =>
   <div>
-    <h1>A B O U T</h1>
+    <span><h1>A B O U T<Link to="/projects"><Icon name="chevron right"/></Link></h1></span>
     <hr />
     <img className="profileImage" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAoLAAAAJDlkYzRmYjVjLTJhNDgtNDEyMy04YzZlLTU0ZmZmZTQ4ODkxNg.jpg" />
     <h3>Some text</h3>
