cb7cc96cea805f8e3ededd80bb8d727b69cb3b4c
shahronak1996@gmail.com <Ronak@MacBook-Air-2.local>
2017-09-20 22:40:12 -0500

    Added GraphQL query for blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,9 +8,34 @@ class BlogPage extends Component {
       <div>
         <h1><Link to="/projects"><Icon name="chevron left"/></Link>B L O G</h1>
         <hr />
+
+        {console.log(this.props.data)}
+        {this.props.data.allMarkdownRemark.edges.map(post => (
+          <Button
+            key={post.node.id}
+            href={post.node.frontmatter.path}>
+            {post.node.frontmatter.title}
+          </Button>
+        ))}
       </div>
     );
   }
 }
+
+export const pageQuery = graphql`
+query indexQUery {
+  allMarkdownRemark(limit: 1000) {
+    edges {
+      node {
+        id
+        frontmatter {
+          title
+          path
+        }
+      }
+    }
+  }
+} 
+`
 
 export default BlogPage;
