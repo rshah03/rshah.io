280aebd6b02ab2aaef4c8e15f4772b2c6622f92b
shahronak1996@gmail.com <Ronak@MacBook-Air-2.attlocal.net>
2017-09-24 16:54:35 -0500

    Content and styling

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Link from 'gatsby-link'
-import { Icon, Grid, Button } from 'semantic-ui-react'
+import { Button, Icon } from 'semantic-ui-react'
 import '../layouts/blog.scss'
 
 class BlogPage extends Component {
@@ -18,7 +18,7 @@ class BlogPage extends Component {
                 className="blogButton"
                 key={post.node.id}
                 href={post.node.frontmatter.path}
-                target="_blank">
+              >
                 {post.node.frontmatter.title}
               </Button>
             ))}
